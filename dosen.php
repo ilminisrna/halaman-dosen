561aa0c413e0922cb64dcac765eaeda418c06994
Alvito Dienova <100193223+Dienova14@users.noreply.github.com>
2022-11-23 21:11:11 +0700

    Issue

--- dosen.php
+++ dosen.php
@@ -1,69 +1,35 @@
 <?php
-    session_start();
+    include 'koneksi.php';
+    $sql = "SELECT * FROM tb_dosen";
+    $result = mysqli_query($conn, $sql);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>dosen</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
-
 <body>
-        <h2>Data Dosen <br></h2>
-        <form action="" method="post">
-            <div class="box">
-                <table>
-                    <tr>
-                        <td> Nama Lengkap</td>
-                        <td>:</td>
-                        <td>
-                            <input type="text" name="nama" class="input-control">
-                        </td>
-                    <tr>
-                        <td> Tempat Lahir</td>
-                        <td>:</td>
-                        <td>
-                            <input type="text" name="tempat_lahir" class="input-control">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> Tanggal Lahir</td>
-                        <td>:</td>
-                        <td>
-                            <input type="date" name="tanggal_lahir" class="input-control">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> Jenis Kelamin</td>
-                        <td>:</td>
-                        <td>
-                            <input type="radio" name="jenis_kelamin" class="input-control" value="Laki-laki"> Laki-laki &nbsp;&nbsp;&nbsp;
-                            <input type="radio" name="jenis_kelamin" class="input-control" value="Perempuan"> Perempuan
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> Agama</td>
-                        <td>:</td>
-                        <td>
-                            <input type="text" name="agama" class="input-control">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> No HP</td>
-                        <td>:</td>
-                        <td>
-                            <input type="text" name="agama" class="input-control">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> Tahun Kelulusan</td>
-                        <td>:</td>
-                        <td>
-                            <input type="text" name="agama" class="input-control">
-                        </td>
-                    </tr>
-            </div>
-    </div>
-    
+<table border ="1">
+        <tr>
+            <th>ID Mahasiswa</th>
+            <th>Nama Mahasiswa</th>
+            <th>Alamat</th>
+            <th>No. Telepon</th>
+        </tr>
+        <?php while($data = mysqli_fetch_row($result)); ?>
+        <tr>
+            <td><?php echo $data[0]; ?></td>
+            <td><?php echo $data[1];?></td>
+            <td><?php echo $data[2]; ?></td>
+            <td><?php echo $data[3]; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
 </body>
 </html>
